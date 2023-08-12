97339cb93846c033d7b082fb96f31e8fb684cb32
Daniel Davenport <ddavenportwork10@gmail.com>
2023-08-12 14:19:40 -0700

    Minor fix
    Fixed being able to place red tile before selecting a tile

--- Assets/Scripts/MazeGenerator.cs
+++ Assets/Scripts/MazeGenerator.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Cysharp.Threading.Tasks;
-using static UnityEditor.PlayerSettings;
 
 public abstract class MazeGenerator : MonoBehaviour
 {
